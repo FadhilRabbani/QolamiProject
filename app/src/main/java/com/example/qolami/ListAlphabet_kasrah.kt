c35001d0979e7570ac4da16cd1299b4ccc83da53
FadhilRabbani <fadhilrabbani002@gmail.com>
2023-11-08 12:11:38 +0700

    initial commit

---
+++ app/src/main/java/com/example/qolami/ListAlphabet_kasrah.kt
@@ -0,0 +1,11 @@
+package com.example.qolami
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListAlphabet_kasrah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_alphabet_kasrah)
+    }
+}
