634cb780d5e689fc5c9f997bc44705382b6ba831
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2023-11-10 03:36:36 +0700

    initial commit

---
+++ app/src/main/java/com/example/qolami/HijaiyahFathah.kt
@@ -0,0 +1,6 @@
+package com.example.qolami
+
+import android.widget.ImageView
+
+data class HijaiyahFathah(val huruf: String, val videoId: String, val gifFileName: ImageView)
+
--- app/src/main/java/com/example/qolami/ListAlphabet_fathah.kt
+++ app/src/main/java/com/example/qolami/ListAlphabet_fathah.kt
@@ -8,4 +8,5 @@ class ListAlphabet_fathah : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_alphabet_fathah)
     }
+
 }
