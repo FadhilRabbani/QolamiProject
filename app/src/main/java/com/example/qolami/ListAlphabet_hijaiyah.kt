c121b2a9f70dd4ddf68ebe25994a96e6dac34282
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2023-11-15 21:03:27 +0700

    initial commit

--- app/src/main/java/com/example/qolami/ListAlphabet_hijaiyah.kt
+++ app/src/main/java/com/example/qolami/ListAlphabet_hijaiyah.kt
@@ -1,12 +1,15 @@
 package com.example.qolami
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 
 class ListAlphabet_hijaiyah : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
     lateinit var backtomainButton_hijaiyah :Button
     lateinit var nextbutton_hijaiyah :ImageButton
     lateinit var prevbutton_hijaiyah :ImageButton
@@ -17,6 +20,13 @@ class ListAlphabet_hijaiyah : AppCompatActivity() {
         backtomainButton_hijaiyah=findViewById(R.id.backtoMain_hijaiyah)
         nextbutton_hijaiyah=findViewById(R.id.nextbtn_hijaiyah)
         prevbutton_hijaiyah=findViewById(R.id.prevbtn_hijaiyah)
+
+
+        for (i in 1..30) {
+            val buttonId = resources.getIdentifier("imageButton$i", "id", packageName)
+            val imageButton = findViewById<ImageButton>(buttonId)
+            imageButton.setOnClickListener { onButtonClick(i) }
+        }
 
         backtomainButton_hijaiyah.setOnClickListener {
             val intent = Intent(this@ListAlphabet_hijaiyah, PracticeActivity::class.java)
@@ -30,5 +40,16 @@ class ListAlphabet_hijaiyah : AppCompatActivity() {
             val intent = Intent(this@ListAlphabet_hijaiyah,ListAlphabet_dhammah::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun onButtonClick(buttonNumber: Int) {
+        val soundId = resources.getIdentifier("sound$buttonNumber", "raw", packageName)
+        mediaPlayer = MediaPlayer.create(this, soundId)
+        mediaPlayer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer.release()
     }
 }
