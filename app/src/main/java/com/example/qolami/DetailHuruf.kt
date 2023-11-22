62afa90e375952301de80510862e1c22f980fb74
FadhilRabbani <fadhilrabbani002@gmail.com>
2023-11-22 16:57:26 +0700

    initial commit

--- app/src/main/java/com/example/qolami/DetailHuruf.kt
+++ app/src/main/java/com/example/qolami/DetailHuruf.kt
@@ -7,5 +7,6 @@ class DetailHuruf : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_huruf)
+        
     }
 }
