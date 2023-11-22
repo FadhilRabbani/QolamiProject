8ae1a17ac962ea132522836c1e6b1c2099f0c462
sutanbaihaqi23 <116020557+sutanbaihaqi23@users.noreply.github.com>
2023-11-22 18:50:09 +0700

    initial commit

--- app/src/main/java/com/example/qolami/DetailHuruf.kt
+++ app/src/main/java/com/example/qolami/DetailHuruf.kt
@@ -27,28 +27,9 @@ class DetailHuruf : AppCompatActivity() {
         wvYoutube.addYouTubePlayerListener(object: AbstractYouTubePlayerListener(){
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 youTubePlayer.loadVideo(huruf.videoId,0f)
-                youTubePlayer.play()
-                youTubePlayer.addListener(object : AbstractYouTubePlayerListener() {
-                    override fun onStateChange(youTubePlayer: YouTubePlayer, state: PlayerConstants.PlayerState) {
-                        if (state == PlayerConstants.PlayerState.PLAYING) {
-                            hideSystemUI()
-                        }
-                    }
-                })
             }
         })
         }
 
     }
-    private fun hideSystemUI() {
-        val decorView: View = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-    }
-
-
 }
